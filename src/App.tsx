bfc3266379d6048812074110b9eeb78a1d2d41a2
Joshua Durham <jdurham38@gmail.com>
2024-10-29 19:53:37 -0400

    initial commit for garage shop

--- src/App.tsx
+++ src/App.tsx
@@ -13,25 +13,25 @@ function App() {
   return (
     <div>            
       <Helmet>
-        <title>G Mac Pizza & Wings</title>
+        <title>Company title</title>
         <meta name="description" content="Homemade pizza made fresh in Phillipsburg, NJ" />
         <meta name="keywords" content="Pizza, Italian, wings, homemade, fresh" />
-        <script>{`
+        {/* <script>{`
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-MRNHRDFM');
-        `}</script>
+        `}</script> */}
       </Helmet>
       <noscript>
-        <iframe 
+        {/* <iframe 
           src="https://www.googletagmanager.com/ns.html?id=GTM-MRNHRDFM"
           height="0" 
           width="0" 
           style={{ display: 'none', visibility: 'hidden' }} 
           title="Google Tag Manager"
-        ></iframe>
+        ></iframe> */}
       </noscript>
       <Header />
       <div id="hero" className={styles.section}><Hero /></div>

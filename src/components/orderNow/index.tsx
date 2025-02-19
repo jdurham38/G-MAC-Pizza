35c26706043918805c55a4025e2bc5d9108ee0de
Josh Durham <60802210+jdurham38@users.noreply.github.com>
2025-02-19 12:16:45 -0500

    Update index.tsx

--- src/components/orderNow/index.tsx
+++ src/components/orderNow/index.tsx
@@ -5,7 +5,7 @@ import styles from "./styles.module.css";
 export default function OrderNow() {
     return(
         <div>
-                <a className={styles.orderNowButton} href="https://slicelife.com/restaurants/nj/phillipsburg/08865/mac-pizza-wings-phillipsburg/menu">Menu</a>
+                <a className={styles.orderNowButton} href="https://mac3.hrpos.heartland.us/order">Menu</a>
         </div>
     );
 }
